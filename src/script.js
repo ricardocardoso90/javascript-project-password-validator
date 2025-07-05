dcc9f1f1ec1881f6bec82145243a1c72365ca6e7
Ricardo Cardoso <ricardomanolo90@gmail.com>
2025-07-05 12:35:13 -0300

    update commit

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
-let passwordInput = document.querySelector('.password');
-let progressBar = document.querySelector('.progress .bar');
+let passwordInput = document.querySelector(".password");
+let progressBar = document.querySelector(".progress .bar");
 
-passwordInput.addEventListener('keyup', () => {
+passwordInput.addEventListener("keyup", () => {
   let strength = 0;
 
   strength += renderRule(
@@ -9,27 +9,21 @@ passwordInput.addEventListener('keyup', () => {
     passwordInput.value.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)
   );
 
-  strength += renderRule(
-    ".one-number",
-    passwordInput.value.match(/([0-9])/)
-  );
+  strength += renderRule(".one-number", passwordInput.value.match(/([0-9])/));
 
   strength += renderRule(
     ".one-special-char",
     passwordInput.value.match(/([#@%&*!_])/)
   );
 
-  strength += renderRule(
-    ".min-char",
-    passwordInput.value.length >= 6,
-  );
+  strength += renderRule(".min-char", passwordInput.value.length >= 6);
 
   let pct = Math.floor((strength / 4) * 100);
   progressBar.style.width = `${pct}%`;
 
-  if(pct === 100) {
-    alert('Campo preenchido com sucesso!! Parabéns.');
-  };
+  if (pct === 100) {
+    alert("Campo preenchido com sucesso!! Parabéns.");
+  }
 
   let ruleArea = document.querySelector(".low-upper-case");
   if (passwordInput.value.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) {
@@ -38,13 +32,13 @@ passwordInput.addEventListener('keyup', () => {
   } else {
     // renderRule('.low-upper-case', false);
     ruleArea.querySelector(".valid-point").style.backgroundColor = "red";
-  };
-
+  }
 });
 
 function renderRule(ruleClass, valid) {
   let ruleArea = document.querySelector(ruleClass);
-  ruleArea.querySelector(".valid-point")
-    .style.backgroundColor = valid ? "green" : "red";
+  ruleArea.querySelector(".valid-point").style.backgroundColor = valid
+    ? "green"
+    : "red";
   return valid ? 1 : 0;
-};
+}
