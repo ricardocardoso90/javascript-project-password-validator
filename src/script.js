ffa822b042fe2b159d1ed367269dd605bb4e5b62
Ricardo Cardoso <ricardomanolo90@gmail.com>
2025-07-21 14:40:38 -0300

    update commit

--- src/script.js
+++ src/script.js
@@ -4,26 +4,15 @@ let progressBar = document.querySelector(".progress .bar");
 passwordInput.addEventListener("keyup", () => {
   let strength = 0;
 
-  strength += renderRule(
-    ".low-upper-case",
-    passwordInput.value.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)
-  );
-
-  strength += renderRule(".one-number", passwordInput.value.match(/([0-9])/));
-
-  strength += renderRule(
-    ".one-special-char",
-    passwordInput.value.match(/([#@%&*!_])/)
-  );
-
   strength += renderRule(".min-char", passwordInput.value.length >= 6);
+  strength += renderRule(".one-number", passwordInput.value.match(/([0-9])/));
+  strength += renderRule(".low-upper-case", passwordInput.value.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/));
+  strength += renderRule(".one-special-char", passwordInput.value.match(/([#@%&*!_])/));
 
   let pct = Math.floor((strength / 4) * 100);
   progressBar.style.width = `${pct}%`;
 
-  if (pct === 100) {
-    alert("Campo preenchido com sucesso!! Parabéns.");
-  };
+  if (pct === 100) { alert("Campo preenchido com sucesso!! Parabéns.") };
 
   let ruleArea = document.querySelector(".low-upper-case");
   if (passwordInput.value.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) {
@@ -37,8 +26,6 @@ passwordInput.addEventListener("keyup", () => {
 
 function renderRule(ruleClass, valid) {
   let ruleArea = document.querySelector(ruleClass);
-  ruleArea.querySelector(".valid-point").style.backgroundColor = valid
-    ? "green"
-    : "red";
+  ruleArea.querySelector(".valid-point").style.backgroundColor = valid ? "green" : "red";
   return valid ? 1 : 0;
 };
